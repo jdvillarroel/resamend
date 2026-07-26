a6983bb5ef9e66cc8bedfc35e6ede0f3e4b4ab1f
Jesus D. Villarroel <jdvillarroel@gmail.com>
2026-07-26 17:47:38 -0500

    Add layout of home page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resumind" },
+    { title: "Resamend" },
     { name: "description", content: "Smart feedback for your dream job." },
   ];
 }
 
 export default function Home() {
   return (
-    <main>
-      <section></section>
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading pb-10">
+          <h1 className="">Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <section className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </section>
+      )}
     </main>
   );
 }
