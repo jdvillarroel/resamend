85a8204f81614f12bbf223916da3e2c7e8006a67
Jesus D. Villarroel <jdvillarroel@gmail.com>
2026-07-26 16:34:52 -0500

    First commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job." },
+  ];
+}
+
+export default function Home() {
+  return (
+    <main>
+      <section></section>
+    </main>
+  );
+}
